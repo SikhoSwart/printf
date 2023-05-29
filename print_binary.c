857038fc1e20f2d1de3fb4eba1ab71b62da06140
SikhoSwart <sikhoswart@gmail.com>
2023-05-29 09:47:55 -0700

    change print_binary

--- print_binary.c
+++ print_binary.c
@@ -6,35 +6,5 @@
  */
 int print_binary(unsigned int b)
 {
-	int num;
-	unsigned int i, j, len, pow;
-
-	if (b != 0)
-	{
-		len = 0;
-		j = b;
-		while (j != 0)
-		{
-			j = j / 2;
-			len++;
-		}
-		pow = 1;
-		for (i = 1; i <= len; i++)
-		{
-			pow = pow * 2;
-		}
-		for (i = 1; i <= len; i++)
-		{
-			l = b / pow;
-			_putchar(l + '0');
-			pow = pow / 2;
-			num++;
-		}
-	}
-	else
-	{
-		_putchar('0');
-		return (1);
-	}
-	return (num);
+	return (print_base(b, "01"));
 }
